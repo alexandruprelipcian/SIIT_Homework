dfabea02c2a1190d430b7f755de42c0fb7b16ff0
jesuswillcome <83041617+jesuswillcome@users.noreply.github.com>
2022-11-22 20:25:49 +0200

    alex preli

--- src/main/java/org/siit/homework/week7/Main.java
+++ src/main/java/org/siit/homework/week7/Main.java
@@ -2,6 +2,6 @@ package org.siit.homework.week7;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the main class for week 7 homework");
+        System.out.println("alex preli");
     }
 }
