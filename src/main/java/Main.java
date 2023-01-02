2e3c6c9aa60f10de3a91d147c809ca305f089475
jesuswillcome <83041617+jesuswillcome@users.noreply.github.com>
2023-01-02 13:29:27 +0200

    Testing - Assignment

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Valid input eg: 100 cm + 20 mm + 10 km");
+        System.out.print("Enter expression with spaces: ");
+        String input = s.nextLine();
+        String[] expression = input.split(" ");
+
+        if (Calculator.calculateExpression(expression) != null) {
+            System.out.println("Expression result: " + Calculator.calculateExpression(expression) + " mm");
+        } else {
+            System.out.println("Invalid expression!");
+        }
+    }
+}
